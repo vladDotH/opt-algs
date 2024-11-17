5550ebe626a70eba401b92ef92a97000c4e2de0e
Vlad <vladbolkunovv@gmail.com>
2024-11-18 00:41:05 +0300

    feat: methods enum

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,3 +1,10 @@
+import enum
+
 from algorithms.chords import *
 from algorithms.gold_slice import *
 from algorithms.newton_rafson import *
+
+class Method(enum.Enum):
+    GOLD_SLICE = 0
+    CHORDS = 1
+    NEWTON_RAFSON = 2
